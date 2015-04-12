db1706dee06466f13bc19b4ac3d55cb26e2a0864
Artem Kolesnikov <sirruf@me.com>
2015-04-13 01:22:31 +0300

    Helpers initializer added.

---
+++ config/initializers/photos.rb
@@ -0,0 +1,39 @@
+# root_path and root_url are defined for the engine, so those still need to be handled differently.
+# This issue could happend for any routes that overlap.
+#
+# (main_app||self).root_path
+#
+# And the other way if you want to link to the engines root_path. blogit is the engine_name.
+#
+# (blogit||self).root_path
+#
+#
+
+module Photos
+  module ApplicationHelper
+    def method_missing method, *args, &block
+      puts "LOOKING FOR ROUTES #{method}"
+      if method.to_s.end_with?('_path') or method.to_s.end_with?('_url')
+        if main_app.respond_to?(method)
+          main_app.send(method, *args)
+        else
+          super
+        end
+      else
+        super
+      end
+    end
+
+    def respond_to?(method)
+      if method.to_s.end_with?('_path') or method.to_s.end_with?('_url')
+        if main_app.respond_to?(method)
+          true
+        else
+          super
+        end
+      else
+        super
+      end
+    end
+  end
+end

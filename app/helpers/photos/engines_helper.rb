20550277e727acb548c8633f491f65cf2e9a20bc
Artem Kolesnikov <sirruf@me.com>
2015-04-13 03:44:49 +0300

    Helpers namespace updated.

--- app/helpers/photos/engines_helper.rb
+++ app/helpers/photos/engines_helper.rb
@@ -1,14 +1,16 @@
-module EnginesHelper
-  def photos_module_enabled?
-    defined?(photos).present?
-  end
+module Photos
+  module EnginesHelper
+    def photos_module_enabled?
+      defined?(photos).present?
+    end
 
-  def resource_link(resource)
-    if resource.present?
-      if resource.is_a?(Photos::Gallery)
-        link_to polymorphic_path([photos, :admin, resource]), polymorphic_path([photos, :admin, resource]) if defined?(photos).present?
-      else
-        link_to polymorphic_path(resource), polymorphic_path([:admin, resource])
+    def resource_link(resource)
+      if resource.present?
+        if resource.is_a?(Photos::Gallery)
+          link_to polymorphic_path([photos, :admin, resource]), polymorphic_path([photos, :admin, resource]) if defined?(photos).present?
+        else
+          link_to polymorphic_path(resource), polymorphic_path([:admin, resource])
+        end
       end
     end
   end

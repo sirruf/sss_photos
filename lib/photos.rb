ffd72f44e3bbf1c9ce3c6e356a3f5e77a6691cc0
Artem Kolesnikov <sirruf@me.com>
2016-09-11 16:38:37 +0300

    0.0.19

--- lib/photos.rb
+++ lib/photos.rb
@@ -2,4 +2,5 @@ require "photos/engine"
 
 module Photos
   mattr_accessor :on_home_support_enabled
+  mattr_accessor :admin_namespace
 end
--- lib/photos/version.rb
+++ lib/photos/version.rb
@@ -1,3 +1,3 @@
 module Photos
-  VERSION = '0.0.18'
+  VERSION = '0.0.19'
 end

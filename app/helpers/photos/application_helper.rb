723d4028dd0ee3c2b5d8ea73aa7181c00e780c9c
Artem Kolesnikov <sirruf@me.com>
2016-09-11 17:13:02 +0300

    0.0.21

--- app/helpers/photos/application_helper.rb
+++ app/helpers/photos/application_helper.rb
@@ -33,7 +33,7 @@ module Photos
 
     def flash_messages(opts = {})
       flash.each do |msg_type, message|
-        concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do
+        concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type.to_sym)} fade in") do
                  concat content_tag(:button, 'x', class: 'close', data: {dismiss: 'alert'})
                  concat message
                end)

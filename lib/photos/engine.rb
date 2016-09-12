5cf23f4aa26d40c7a533fddbf40534a6d2b476f5
Artem Kolesnikov <sirruf@me.com>
2016-09-12 11:44:27 +0300

    0.0.25

--- lib/photos/engine.rb
+++ lib/photos/engine.rb
@@ -1,13 +1,9 @@
 module Photos
   class Engine < ::Rails::Engine
     isolate_namespace Photos
-    # config.to_prepare do
-    #   ApplicationController.helper(ActionView::Helpers::ApplicationHelper)
-    # end
-    # config.to_prepare do
-    #   ApplicationController.helper(EnginesHelper)
-    #   ApplicationController.helper(FlashHelper)
-    # end
+    config.to_prepare do
+      Photos::ApplicationController.helper Rails.application.helpers
+    end
   end
   require 'rubygems'
   require 'russian'
--- lib/photos/version.rb
+++ lib/photos/version.rb
@@ -1,3 +1,3 @@
 module Photos
-  VERSION = '0.0.24'
+  VERSION = '0.0.25'
 end
